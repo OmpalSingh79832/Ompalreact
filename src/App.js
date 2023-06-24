f2542b7654620b60f7f1a2ec96ac3290ece3641d
Syed <83526948+syed8867@users.noreply.github.com>
2023-06-24 13:40:33 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import "./App.css";
+import Favorites from "./components/Favorites";
+import Meals from "./components/Meals";
+import Modal from "./components/Modal";
+import Search from "./components/Search";
+import { useGlobalContext } from "./context";
+import Hero from "./components/Hero";
+
+function App() {
+  const { showModal, favorites, showFavorite } = useGlobalContext();
+
+  return (
+    <main>
+      <header>
+        <Search />
+        {showFavorite && favorites.length >= 1 && <Favorites />}
+      </header>
+
+      <Hero />
+
+      <Meals />
+      {showModal && <Modal />}
+    </main>
+  );
+}
+
+export default App;
